b8f71e9ff068e63319913002592e8a54745ceee5
zjsan <zjsantos25@gmail.com>
2025-10-02 16:05:55 +0800

    initialized express and middlewares

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
+
+const app = express();
+const port = 3000; //application port
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
