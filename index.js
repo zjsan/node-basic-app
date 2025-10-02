1f55d538fb3a93dd1b037b58df8ce299fe786c15
zjsan <zjsantos25@gmail.com>
2025-10-02 15:55:31 +0800

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+//importing modules
+const express = require("express");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
